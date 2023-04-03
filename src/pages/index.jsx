f3dd7f36c1ad534fb44df9297c705edaa71e853f
maxteng92 <46652088+maxteng92@users.noreply.github.com>
2023-04-03 18:44:40 +0800

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function RootLayout() {
 return (
 <div>
-<Link to="/">Home</Link> | <Link to="/about">About</Link>
+<Link to="/">自我介紹</Link> | <Link to="/about">About</Link>
 <div>
 <Outlet />
 </div>
@@ -10,5 +10,5 @@ return (
 );
 }
 export function RootIndex() {
-return <h1>Home</h1>;
+return <h1>我是B10401026鄧立珏</h1>;
 }
